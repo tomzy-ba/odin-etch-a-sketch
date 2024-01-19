44942081b0d90b8221929ccccb56fff390cffa53
tomzy-ba <trbee100@gmail.com>
2024-01-19 17:16:10 +0000

    made div turn into square

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let changeSizeButton = document.querySelector("#changesizebutton");
 changeSizeButton.addEventListener("click",() => gridChangeSize())
 
 function gridChangeSize() {
-    prompt("hello")
+    gridMaker(prompt("enter the number"))
 }
 
 
-function gridMaker() {
-for (let i = 1; i <= 256; i++) {
+function gridMaker(gridSize) {
+    gridSize = gridSize * gridSize;
+    gridContainer.innerHTML = "";
+    gridContainer.style.width = gridSize + "px";
+    gridContainer.style.height = gridSize + "px";
+for (let i = 1; i <= gridSize; i++) {
     let griditemx = document.createElement("div");
+    griditemx.textContent =""
     griditemx.classList.add('griditemclass');
 
     gridContainer.append(griditemx);
 
 }
 }
-gridMaker()
+gridMaker(16)
