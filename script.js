f55bf676d433bc52002f3563854a83bec7267145
tomzy-ba <trbee100@gmail.com>
2024-01-19 15:11:08 +0000

    made grid work

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gridContainer = document.querySelector("#grid-container");
 
-for (let i = 0; i <= 256; i++) {
+for (let i = 1; i <= 256; i++) {
     let griditemx = document.createElement("div");
-    griditemx.textContent = i + "";
+    griditemx.textContent ="x";
+
     gridContainer.append(griditemx);
 
 }
