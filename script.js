dfbcf9822f52f7718c120940c99306612b3ab217
tomzy-ba <trbee100@gmail.com>
2024-01-19 15:17:06 +0000

    added class to divs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gridContainer = document.querySelector("#grid-container");
 for (let i = 1; i <= 256; i++) {
     let griditemx = document.createElement("div");
     griditemx.textContent ="x";
+    griditemx.classList.add('griditemclass')
 
     gridContainer.append(griditemx);
 
